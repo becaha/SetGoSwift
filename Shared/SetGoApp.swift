1f20720439d6a828ec20e549be0522a86e3db0ca
Rebecca Haggard <beccahagg5@gmail.com>
2020-09-17 15:52:53 -0600

    Initial Commit

---
+++ Shared/SetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGoApp.swift
+//  Shared
+//
+//  Created by Rebecca Nybo on 9/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
